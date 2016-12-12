62ff7dcb08848f5874e7f24f861ab00378ff8aaa
JoshDreamland <JoshV@zoominternet.net>
2016-12-11 21:43:29 -0700

    Commit this site, since I want to edit it occasionally

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Dreamland: Home of Josh</title>
+    <link rel="stylesheet" type="text/css" href="style.css"></link>
+  </head>
+  <body>
+   <div class="bodydiv">
+    <div class="textdiv">
+    <p>As I intended for the TLD to suggest, this is an Instant-Messaging-related personal domain.
+    I use it basically exclusively for the email address, but intend to populate it "at some point in the
+    future" with my interests.</p>
+
+    <p>Most of the stuff I do is available on <a href="https://github.com/JoshDreamland">my GitHub page</a>.
+
+    <p>I think I will also write <a href="about.php">a little about me</a>.
+
+    <p>«Something about tacos»</p>
+    </div>
+   </div>
+  </body>
+</html>
